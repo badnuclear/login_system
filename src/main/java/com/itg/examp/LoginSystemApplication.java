6b9158344dd30cac4deed09bfea6289bc38a54a9
bad_nuclear <alsrbs4024@gmail.com>
2025-01-21 13:23:24 +0900

    스프링 배치 작업

--- src/main/java/com/itg/examp/LoginSystemApplication.java
+++ src/main/java/com/itg/examp/LoginSystemApplication.java
@@ -1,9 +1,12 @@
 package com.itg.examp;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LoginSystemApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/itg/examp/test/LoginSystemApplicationTests.java
+++ src/test/java/com/itg/examp/test/LoginSystemApplicationTests.java
@@ -1,9 +1,10 @@
 package com.itg.examp.test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class LoginSystemApplicationTests {
 
 	@Test
